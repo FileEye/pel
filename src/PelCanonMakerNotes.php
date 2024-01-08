6eff05507033a1c323323dfe6e8e2cd025c02a9f
mondrake <mondrake.org@gmail.com>
2024-01-08 19:11:14 +0100

    XII

--- src/PelCanonMakerNotes.php
+++ src/PelCanonMakerNotes.php
@@ -130,31 +130,6 @@ class PelCanonMakerNotes extends PelMakerNotes
         0x0004
     ];
 
-    private $undefinedPicInfoTags = [
-        0x0001,
-        0x0006,
-        0x0007,
-        0x0008,
-        0x0009,
-        0x000a,
-        0x000b,
-        0x000c,
-        0x000d,
-        0x000e,
-        0x000f,
-        0x0010,
-        0x0011,
-        0x0012,
-        0x0013,
-        0x0014,
-        0x0015,
-        0x0017,
-        0x0018,
-        0x0019,
-        0x001b,
-        0x001c
-    ];
-
     private $undefinedFileInfoTags = [
         0x0002,
         0x000a,
