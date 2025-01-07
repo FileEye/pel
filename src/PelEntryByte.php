71bc7387473cc2681cb574fa16253cd7d122be75
Cristi Radu <indy2kro@gmail.com>
2025-01-07 15:10:34 +0200

    Major update - added strict types, increased phpstan to level 8, PSR12 support (#31)
    * Update tests.yml
    
    * Update tests.yml
    
    * Updated project to be compliant with phpstan level 6
    
    * Update PelIfd.php
    
    * phpcs fix
    
    * Updated phpstan config
    
    * Changed order of github actions operations
    
    * Updated phpstan to level 7
    
    * Updated phpstan to level 8
    
    * Added rector support
    
    * Removed support for php 8.1
    
    * Added declare(strict_types=1)
    
    * Update PelIllegalFormatException.php
    
    * Added phpinsights
    
    * Rector fix
    
    * Update tests.yml
    
    * Updated phpcs to use PSR12 standard
    
    * Update phpinsights.php
    
    * Updated examples
    
    * Added support for file streams to avoid memory issues when reading large files
    
    * Updated phpinsights config, added more tests
    
    * Added more tests
    
    * Added more tests
    
    * Updated tests
    
    * Create .gitattributes
    
    * Update .gitattributes
    
    * Readd 8.1, fail PHPUnit on warnings
    
    * Update tests.yml
    
    * Fixed phpunit warnings
    
    * Updated composer deps for php 8.1
    
    * Update PelFileStream.php
    
    * Updated deps for php 8.1 (hopefully)
    
    * Removed composer.lock to avoid issues with different versions
    
    * Removed git actions cache
    
    ---------
    
    Co-authored-by: mondrake <mondrake.org@gmail.com>

--- src/PelEntryByte.php
+++ src/PelEntryByte.php
@@ -1,39 +1,8 @@
 <?php
 
-/**
- * PEL: PHP Exif Library.
- * A library with support for reading and
- * writing all Exif headers in JPEG and TIFF images using PHP.
- *
- * Copyright (C) 2004, 2005, 2006, 2007 Martin Geisler.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program in the file COPYING; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA 02110-1301 USA
- */
+declare(strict_types=1);
 
-/**
- * Classes used to hold bytes, both signed and unsigned.
- * The {@link
- * PelEntryWindowsString} class is used to manipulate strings in the
- * format Windows XP needs.
- *
- * @author Martin Geisler <mgeisler@users.sourceforge.net>
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public
- *          License (GPL)
- * @package PEL
- */
+namespace lsolesen\pel;
 
 /**
  * Class for holding unsigned bytes.
@@ -41,20 +10,13 @@
  * This class can hold bytes, either just a single byte or an array of
  * bytes. The class will be used to manipulate any of the Exif tags
  * which has format {@link PelFormat::BYTE}.
- *
- * @author Martin Geisler <mgeisler@users.sourceforge.net>
- * @package PEL
  */
-namespace lsolesen\pel;
-
 class PelEntryByte extends PelEntryNumber
 {
-
     /**
      * Make a new entry that can hold an unsigned byte.
      *
-     * The method accept several integer arguments. The {@link
-     * getValue} method will always return an array except for when a
+     * The method accept several integer arguments. The {@link getValue} method will always return an array except for when a
      * single integer argument is given here.
      *
      * @param int $tag
@@ -68,7 +30,7 @@ class PelEntryByte extends PelEntryNumber
      *            unsigned byte, that is between 0 and 255 (inclusive). If not,
      *            then a {@link PelOverflowException} will be thrown.
      */
-    public function __construct($tag, $value = null)
+    public function __construct(int $tag, ?int $value = null)
     {
         $this->tag = $tag;
         $this->min = 0;
@@ -81,11 +43,11 @@ class PelEntryByte extends PelEntryNumber
     }
 
     /**
-     *
      * {@inheritdoc}
+     *
      * @see \lsolesen\pel\PelEntryNumber::numberToBytes()
      */
-    public function numberToBytes($number, $order)
+    public function numberToBytes(int $number, bool $order): string
     {
         return chr($number);
     }
